2391d5a667e469b4731db4fdf84789b228fdf2ea
deveusopht <dev.eusopht@gmail.com>
2022-07-05 12:20:51 +0500

    images added

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:web_practice/screens/home.dart';
 
 void main() {
   runApp(const WebPractice());
@@ -9,7 +10,7 @@ class WebPractice extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       home: Home(),
     );
   }
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,3 +1,10 @@
+import 'dart:developer' as dev;
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:web_practice/utils/assets.dart';
+import 'package:web_practice/utils/size_config.dart';
+
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
 
@@ -6,8 +13,72 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  int imageNo = 0;
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    SizeConfig.init(context);
+    return Stack(
+      children: [
+        // SizedBox(
+        //   width: SizeConfig.width,
+        //   height: SizeConfig.height,
+        //   child: FadeInImage.assetNetwork(
+        //     placeholder: Assets.images[imageNo],
+        //     image: Assets.images[imageNo],
+        //   ),
+        // ),
+        AnimatedSwitcher(
+          duration: const Duration(
+            milliseconds: 400,
+          ),
+          child: Image.asset(
+            Assets.images[imageNo],
+            fit: BoxFit.fill,
+            width: SizeConfig.width,
+            height: SizeConfig.height,
+          ),
+        ),
+        SizedBox(
+          width: SizeConfig.width,
+          height: SizeConfig.height,
+          child: Row(
+            children: [
+              Expanded(
+                child: Container(),
+              ),
+              Expanded(
+                child: getImageButtons(),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget getImageButtons() {
+    return Column(
+      children: [
+        ElevatedButton(
+          onPressed: () {
+            setState(() {
+              imageNo = getRandom();
+              dev.log('Image:$imageNo');
+            });
+          },
+          child: Text('Change Image'),
+        ),
+      ],
+    );
+  }
+
+  int getRandom() {
+    final random = Random();
+    int temp = random.nextInt(4);
+    if (temp == imageNo) {
+      return getRandom();
+    }
+    return temp;
   }
 }
